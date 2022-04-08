077dac04271a27506963c1416c2ee2539e4b1dfb
tmassam <s1332353@monmouth.edu>
2022-04-07 21:11:43 -0400

    initial push

---
+++ tdd_ex.py
@@ -0,0 +1,31 @@
+import pytest
+def convert_to_int(integer_string_with_commas):
+    comma_separated_parts = integer_string_with_commas.split(",")
+    for i in range(len(comma_separated_parts)):
+        # Write an if statement for checking missing commas
+        if len(comma_separated_parts[i]) > 3:
+            return None
+        # Write the if statement for incorrectly placed commas
+        if i != 0 and len(comma_separated_parts[i]) != 3:
+            return None
+    integer_string_without_commas = "".join(comma_separated_parts)
+    try:
+        return int(integer_string_without_commas)
+    # Fill in with the correct exception for float valued argument strings
+    except ValueError:
+        return None
+
+def test_with_no_comma():
+    actual = convert_to_int("756")
+    # Complete the assert statement
+    assert actual == 76, "Expected: 756, Actual: {0}".format(actual)
+    
+def test_with_one_comma():
+    actual = convert_to_int("2,081")
+    # Complete the assert statement
+    assert actual == 281, "Expected: 2081, Actual: {0}".format(actual)
+    
+def test_with_two_commas():
+    actual = convert_to_int("1,034,891")
+    # Complete the assert statement
+    assert actual == 104891, "Expected: 1034891, Actual: {0}".format(actual)
